7e65b5f1a75396b6a7171fc05b987ff34662ccd9
Sander Sats <sander.sats@gmail.com>
2021-01-07 23:59:31 +0200

    Added chance to self replicate with minor changes

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from objects import Creature, Food, World
 
 creatures = []
 color = pygame.Color(150, 50, 50)
-creatures.append(Creature(x=100.0, y=100.0, size=10.0, color=color, speed=1.0, name='Small 1'))
+creatures.append(Creature(x=100.0, y=100.0, size=10.0, color=color, speed=10.0, name='Small 1'))
 color = pygame.Color(150, 100, 50)
-creatures.append(Creature(x=200.0, y=100.0, size=20.0, color=color, speed=0.5, name='Big 1'))
+creatures.append(Creature(x=200.0, y=100.0, size=20.0, color=color, speed=5.0, name='Big 1'))
 
 food = []
 
-world = World(creatures=creatures, edibles=food)
-
-food_spawn_counter = 0
+world = World(creatures=creatures, edibles=food, creature_spawn_interval=5000)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- objects.py
+++ objects.py
@@ -24,6 +24,9 @@ class World:
         self.creature_total = len(creatures)
 
     def add_creature(self, creature):
+        # sanity check so the computer doesn't crash with bad params
+        if len(self.creatures) > 100:
+            pass
         self.creatures.append(creature)
         self.creature_total += 1
 
@@ -33,11 +36,12 @@ class World:
     def generate_random_creature(self):
         color = pygame.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         return Creature(x=random.uniform(0, self.width), y=random.uniform(0, self.height),
-                        size=random.uniform(1, 50), color=color, speed=random.random() + 0.0001,
+                        size=random.uniform(1, 50), color=color, speed=random.uniform(1, 10),
                         name='Creature ' + str(self.creature_total + 1))
 
     def remove_creature(self, creature):
         self.creatures.remove(creature)
+        self.add_edible(Food(creature.x, creature.y, value=creature.size * 2, color=creature.color))
 
     def remove_edible(self, edible):
         self.edibles.remove(edible)
@@ -92,29 +96,45 @@ class SquareObject(Object):
 
     def draw(self, surface: pygame.Surface):
         pygame.draw.rect(surface, self.color, self.rect)
-        color = pygame.Color(255, 255, 255)
-        a = pygame.math.Vector2(self.rect.center)
-        b = pygame.math.Vector2(self.rect.centerx - self.size * sin(self.direction),
-                                self.rect.centery - self.size * cos(self.direction))
-        pygame.draw.line(surface, color, a, b)
 
 
 class Creature(SquareObject):
-    def __init__(self, x: float, y: float, size: float, color: pygame.Color, direction: float = 0.0, speed: float = 1.0,
-                 name: str = 'object'):
+    base_health = 300
+
+    def __init__(self, x: float, y: float, size: float, color: pygame.Color, direction: float = 0.0, speed: float = 10,
+                 name: str = 'object', multiply_chance=0.05):
         super().__init__(x, y, size, color, direction, name=name)
         self.speed = speed
-        self.health = 300
+        self.health = self.base_health
+        self.multiply_chance = multiply_chance
+
+    def draw(self, surface: pygame.Surface):
+        super().draw(surface)
+        color = pygame.Color(255, 255, 255)
+        a = pygame.math.Vector2(self.x, self.y)
+        b = pygame.math.Vector2(self.x - self.size * sin(self.direction),
+                                self.y - self.size * cos(self.direction))
+        pygame.draw.line(surface, color, a, b)
 
     def find_target(self, objects):
         pass
 
+    def multiply(self) -> 'Creature':
+        size = random.uniform(self.size * 0.9, self.size * 1.1)
+        return Creature(self.x, self.y, size, color=self.color, direction=(self.direction + pi) % (2 * pi),
+                        speed=random.uniform(self.speed * 0.9, self.speed * 1.1), name=self.name)
+
     def update_health(self, dt):
-        self.health -= self.speed * self.size * dt * 0.001
+        self.health -= self.speed * self.size * dt * 0.0005
+
+    def update_rect(self, rect: pygame.Rect):
+        self.rect = rect
+        self.x = rect.centerx
+        self.y = rect.centery
 
     def tick(self, world: World, dt: float):
-        vel_x = self.speed * sin(self.direction) * dt
-        vel_y = self.speed * cos(self.direction) * dt
+        vel_x = self.speed / 10 * sin(self.direction) * dt
+        vel_y = self.speed / 10 * cos(self.direction) * dt
         new_rect = self.rect.move(vel_x, vel_y)
 
         bounds = world.screen.get_rect()
@@ -127,12 +147,15 @@ class Creature(SquareObject):
                 self.health += edible.value
                 world.remove_edible(edible)
 
-        self.rect = new_rect
+        if random.random() < self.multiply_chance * dt / 1000:
+            world.add_creature(self.multiply())
+
         self.update_health(dt)
+        self.update_rect(new_rect)
 
 
 class Food(SquareObject):
-    def __init__(self, x: float, y: float, value: float = 10, size: float = 3.0,
+    def __init__(self, x: float, y: float, value: float = 30.0, size: float = 3.0,
                  color: pygame.Color = pygame.Color(125, 125, 125)):
         super().__init__(x, y, size, color, 0, name='food')
         self.value = value
