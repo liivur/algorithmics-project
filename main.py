f377169f8572fa12c00456f1bec238a3fc21580e
sivomke <ocean1641@gmail.com>
2021-01-18 17:59:14 +0200

    - dx, dy visualization - corrected velocity function - vision field visualization - vision_radius now also a part of DNA - modified food value

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for i in range(pop_size):
     y = random.randint(0, 768)
     # color red, color green, color blue, speed, size, reproduction preference
     gene = [color.r / 255, color.g / 255, color.b / 255, random.uniform(0, 1), random.uniform(0, 1),
-            random.uniform(0, 1)]
+            random.uniform(0, 1), random.uniform(0, 1)]
     print(f"gene: {gene} generated")
     creatures.append(obj.DnaCreature(x=x, y=y, dna=DNA(gene), name='DnaCreature_' + str(obj.creature_id_generator.get_next_id())))
 
